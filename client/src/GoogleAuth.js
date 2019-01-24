f39ee4520e06ab315943053dcebe4716f7632c79
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2019-01-24 09:59:08 -0500

    form validation

--- client/src/GoogleAuth.js
+++ client/src/GoogleAuth.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import { clientId } from './apis/ClientID';
+import { connect } from 'react-redux';
+import { signInaction, signOutaction } from './actions';
 
 class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load('client: auth2', () => {
-      window.gapi.client.init({
-        clientId: clientId.id,
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId: clientId.id,
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.signInListener(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.signInListener);
+        });
     });
   }
+  signInListener = isSignedIn => {
+    if (isSignedIn) {
+      return this.props.signInaction(this.auth.currentUser.get().getId());
+    } else {
+      return this.props.signOutaction();
+    }
+  };
+
+  onSignInClick = () => {
+    return this.auth.signIn();
+  };
+  onSignOutClick = () => {
+    return this.auth.signOut();
+  };
+
+  renderAuth() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="google" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui red google button" onClick={this.onSignInClick}>
+          <i className="google icon" />
+          Sign In
+        </button>
+      );
+    }
+  }
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuth()}</div>;
   }
 }
-export default GoogleAuth;
+const mapState = state => {
+  return { isSignedIn: state.authReducer.isSignedIn };
+};
+export default connect(
+  mapState,
+  { signInaction, signOutaction }
+)(GoogleAuth);
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  handleError({ touched, error }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="ui header">{error}</div>
+        </div>
+      );
+    }
+  }
+  handleChange = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        {/* {touched && (error && <span>{error}</span>)} */}
+        <div>{this.handleError(meta)}</div>
+        <br />
+      </div>
+    );
+  };
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+  render() {
+    // console.log(this.props);
+    return (
+      <form
+        className="ui form error"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field
+          name="title"
+          component={this.handleChange}
+          label="Enter Your Name"
+        />
+        <Field
+          name="description"
+          component={this.handleChange}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = 'Enter your name';
+  }
+  if (!formValues.description) {
+    errors.description = 'Enter your Description';
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);
